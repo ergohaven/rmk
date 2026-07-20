a0426289a5fe6dc01a64c837f25936bf5277a4a5
kissetfall <j@ergohaven.xyz>
2026-07-20 20:06:39 +0300

    diag: run USB after generated K04 initialization

--- keyboards/k04/src/central.rs
+++ keyboards/k04/src/central.rs
@@ -7,22 +7,11 @@ use rmk::macros::rmk_central;
 
 #[rmk_central]
 mod keyboard_central {
-    #[Overwritten(ChipInit)]
-    fn usb_only_chip_init() {
-        let mut config = ::embassy_nrf::config::Config::default();
-        config.dcdc.reg0_voltage = Some(::embassy_nrf::config::Reg0Voltage::_3V3);
-        config.dcdc.reg0 = true;
-        config.dcdc.reg1 = true;
-        let p = ::embassy_nrf::init(config);
-
-        ::embassy_nrf::pac::CLOCK.tasks_hfclkstart().write_value(1);
-        while ::embassy_nrf::pac::CLOCK.events_hfclkstarted().read() != 1 {}
-    }
-
-    #[Overwritten(Entry)]
+    #[Override(entry)]
     async fn initialization_probe_entry() {
         use ::rmk::core_traits::Runnable;
 
+        let _stack: &::rmk::Stack<'_, _, ::rmk::DefaultPacketPool> = &stack;
         let mut usb_transport = ::rmk::usb::UsbTransport::new(driver, rmk_config.device_config);
         usb_transport.run().await;
     }
