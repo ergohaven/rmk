8a8f0aa16e579665e3c6cbd8b8062c6886a511f9
kissetfall <j@ergohaven.xyz>
2026-07-20 19:16:58 +0300

    diag: isolate generated K04 initialization

--- keyboards/k04/src/central.rs
+++ keyboards/k04/src/central.rs
@@ -1,66 +1,29 @@
-#![no_std]
 #![no_main]
+#![no_std]
 
-use embassy_executor::Spawner;
-use embassy_nrf::{bind_interrupts, nvmc::Nvmc, peripherals, usb};
-use nrf_mpsl as _;
-use rmk::config::{BehaviorConfig, DeviceConfig, PositionalConfig, StorageConfig};
-use rmk::core_traits::Runnable;
-use rmk::types::action::{EncoderAction, KeyAction};
-use rmk::KeymapData;
-use {defmt_rtt as _, panic_probe as _};
+mod layer_names;
 
-const ROW: usize = 10;
-const COL: usize = 6;
-const NUM_LAYER: usize = 16;
-const NUM_ENCODER: usize = 2;
-const DEFAULT_KEYS: [[[KeyAction; COL]; ROW]; NUM_LAYER] = [[[rmk::k!(No); COL]; ROW]; NUM_LAYER];
-const DEFAULT_ENCODERS: [[EncoderAction; NUM_ENCODER]; NUM_LAYER] =
-    [[rmk::encoder!(rmk::k!(No), rmk::k!(No)); NUM_ENCODER]; NUM_LAYER];
+use rmk::macros::rmk_central;
 
-bind_interrupts!(struct Irqs {
-    USBD => usb::InterruptHandler<peripherals::USBD>;
-    CLOCK_POWER => usb::vbus_detect::InterruptHandler;
-});
+#[rmk_central]
+mod keyboard_central {
+    #[Overwritten(ChipInit)]
+    fn usb_only_chip_init() {
+        let mut config = ::embassy_nrf::config::Config::default();
+        config.dcdc.reg0_voltage = Some(::embassy_nrf::config::Reg0Voltage::_3V3);
+        config.dcdc.reg0 = true;
+        config.dcdc.reg1 = true;
+        let p = ::embassy_nrf::init(config);
 
-#[embassy_executor::main]
-async fn main(_spawner: Spawner) {
-    let mut nrf_config = embassy_nrf::config::Config::default();
-    nrf_config.dcdc.reg0_voltage = Some(embassy_nrf::config::Reg0Voltage::_3V3);
-    nrf_config.dcdc.reg0 = true;
-    nrf_config.dcdc.reg1 = true;
-    let p = embassy_nrf::init(nrf_config);
+        ::embassy_nrf::pac::CLOCK.tasks_hfclkstart().write_value(1);
+        while ::embassy_nrf::pac::CLOCK.events_hfclkstarted().read() != 1 {}
+    }
 
-    embassy_nrf::pac::CLOCK.tasks_hfclkstart().write_value(1);
-    while embassy_nrf::pac::CLOCK.events_hfclkstarted().read() != 1 {}
+    #[Overwritten(Entry)]
+    async fn initialization_probe_entry() {
+        use ::rmk::core_traits::Runnable;
 
-    let flash = rmk::storage::async_flash_wrapper(Nvmc::new(p.NVMC));
-    let storage_config = StorageConfig {
-        num_sectors: 32,
-        start_addr: 0,
-        clear_storage: false,
-        clear_layout: true,
-    };
-    let mut behavior_config = BehaviorConfig::default();
-    let positional_config = PositionalConfig::<ROW, COL>::default();
-    let mut keymap_data = KeymapData::new_with_encoder(DEFAULT_KEYS, DEFAULT_ENCODERS);
-    let (_keymap, _storage) = rmk::initialize_keymap_and_storage(
-        &mut keymap_data,
-        flash,
-        &storage_config,
-        &mut behavior_config,
-        &positional_config,
-    )
-    .await;
-
-    let driver = usb::Driver::new(p.USBD, Irqs, usb::vbus_detect::HardwareVbusDetect::new(Irqs));
-    let device_config = DeviceConfig {
-        vid: 0xE126,
-        pid: 0x0071,
-        manufacturer: "Ergohaven",
-        product_name: "K:04 RMK storage probe",
-        serial_number: "diag-rmk-storage-1c285672",
-    };
-    let mut transport = rmk::usb::UsbTransport::new(driver, device_config);
-    transport.run().await;
+        let mut usb_transport = ::rmk::usb::UsbTransport::new(driver, rmk_config.device_config);
+        usb_transport.run().await;
+    }
 }
